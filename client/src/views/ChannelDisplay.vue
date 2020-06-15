8f06e7ef28616ae88b44ad0c5ecb2494f819afed
johnpyp <johnpaulpenaloza@gmail.com>
2020-06-15 16:16:03 -0400

    Moved to table format

--- client/src/views/ChannelDisplay.vue
+++ client/src/views/ChannelDisplay.vue
@@ -18,28 +18,63 @@
         <span class="mr-2 font-semibold">Max viewers</span>
         {{ sully.maxviewers.toLocaleString() }}
       </p>
-      <div v-for="[chan, sim, viewers] in similarities" :key="chan">
-        <div class="relative mb-2">
-          <div class="flex text-gray-700">
-            <div class="mr-2 font-semibold">{{ capitalize(chan) }}:</div>
-            <div>{{ (sim * 100).toFixed(2) }}</div>
-            <div class="flex-grow"></div>
-            <div>{{ viewers.toLocaleString() }}</div>
-          </div>
-          <div>
-            <div
-              class="h-4 text-gray-700 bg-red-400"
-              :style="`width: ${sim * 100}%`"
-            ></div>
-          </div>
-        </div>
-      </div>
+      <table class="w-full mt-8 table-auto">
+        <thead>
+          <tr>
+            <th class="px-2 py-2 text-left">
+              <button
+                class="inline px-1 font-bold border-b border-transparent hover:border-gray-500"
+                @click="changeSort('channel')"
+              >
+                Channel
+                <span v-if="sortBy === 'channel'">
+                  <span v-if="sortDir === 'asc'">⌃</span>
+                  <span v-else>⌄</span>
+                </span>
+              </button>
+            </th>
+            <th class="px-2 py-2 text-left">
+              <button
+                class="inline px-1 font-bold border-b border-transparent hover:border-gray-500"
+                @click="changeSort('similarityScore')"
+              >
+                Similarity Score
+                <span v-if="sortBy === 'similarityScore'">
+                  <span v-if="sortDir === 'asc'">⌃</span>
+                  <span v-else>⌄</span>
+                </span>
+              </button>
+            </th>
+            <th class="px-2 py-2 text-left">
+              <button
+                class="inline px-1 font-bold border-b border-transparent hover:border-gray-500"
+                @click="changeSort('sharedViewers')"
+              >
+                Unique Shared Viewers
+                <span v-if="sortBy === 'sharedViewers'">
+                  <span v-if="sortDir === 'asc'">⌃</span>
+                  <span v-else>⌄</span>
+                </span>
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="row in sortedSimilarites" :key="row.channel">
+            <td class="px-4 py-2 border">{{ capitalize(row.channel) }}</td>
+            <td class="px-4 py-2 border">
+              {{ (row.similarityScore * 100).toFixed(2) }}
+            </td>
+            <td class="px-4 py-2 border">{{ row.sharedViewers }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
-import { capitalize, zip } from "lodash-es";
+import { capitalize, zip, sortBy } from "lodash-es";
 import superagent from "superagent";
 
 const baseUrl = process.env.VUE_APP_API;
@@ -55,11 +90,18 @@ export default {
     return {
       similarities: [],
       sully: null,
+      sortDir: "desc",
+      sortBy: "similarityScore",
     };
   },
   computed: {
     displayChannel() {
       return capitalize(this.channel);
+    },
+    sortedSimilarites() {
+      const sorted = sortBy(this.similarities, this.sortBy);
+      if (this.sortDir === "desc") sorted.reverse();
+      return sorted;
     },
   },
   async mounted() {
@@ -71,10 +113,21 @@ export default {
       s.channel,
       s.similarity.map((x) => Math.sqrt(x)),
       s.shared_unique_viewers
-    );
+    ).map(([channel, similarityScore, sharedViewers]) => ({
+      channel,
+      similarityScore,
+      sharedViewers,
+    }));
     this.sully = body.sullygnome_stats;
   },
   methods: {
+    changeSort(field) {
+      if (this.sortBy === field) {
+        if (this.sortDir === "desc") this.sortDir = "asc";
+        else if (this.sortDir === "asc") this.sortDir = "desc";
+      }
+      this.sortBy = field;
+    },
     capitalize,
   },
 };
--- server/src/index.js
+++ server/src/index.js
@@ -10,15 +10,19 @@ app.use(morgan("short"));
 
 const PORT = 5005;
 
-const { data } = fs.readJsonSync("data.json");
+const json = fs.readJsonSync("data.json");
 
 app.get("/channels", async (req, res) => {
-  res.status(200).json(Object.keys(data));
+  res.status(200).json(Object.keys(json.data));
 });
 
 app.get("/data/:channel", async (req, res) => {
   const { channel } = req.params;
-  res.status(200).json(data[channel]);
+  res.status(200).json(json.data[channel]);
+});
+
+app.get("/latex", async (req, res) => {
+  res.status(200).json(json["technical_description"]);
 });
 
 app.listen(PORT, () => {
